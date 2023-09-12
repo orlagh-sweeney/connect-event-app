59f65182171de639b51c7c4f915bd65172b81805
orlagh-sweeney <orlagh16@gmail.com>
2023-09-12 18:16:54 +0000

    Update styling of navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,17 +8,17 @@ const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand className={styles.logo} href="#home">ConnectBcn</Navbar.Brand>
+        <Navbar.Brand className={styles.Brand} href="#home">ConnectBcn</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
+            <Nav.Link className={styles.NavLink}>
               <i className="fa-solid fa-house"></i>Home
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link className={styles.NavLink}>
               <i className="fa-solid fa-right-to-bracket"></i>Sign In
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link className={styles.NavLink}>
               <i className="fa-solid fa-user-plus"></i>Sign Up
             </Nav.Link>
           </Nav>
