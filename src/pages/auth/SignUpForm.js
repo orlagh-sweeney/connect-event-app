2739d6a9c50e093c27c8c1f16a46627bd384ca66
orlagh-sweeney <orlagh16@gmail.com>
2023-09-13 12:41:26 +0000

    Add logic to handle user sign up

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,20 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
-import appStyles from "../../App.module.css";
 
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import { Form, Button, Col, Row, Container, Alert } from "react-bootstrap";
+
+import axios from "axios";
 
 const SignUpForm = () => {
+    const [signUpData, setSignUpData] = useState({
+        username: "",
+        password1: "",
+        password2: "",
+      });
+      const { username, password1, password2 } = signUpData;
+    
+      const [errors, setErrors] = useState({});
+    
+      const history = useHistory();
+    
+      const handleChange = (event) => {
+        setSignUpData({
+          ...signUpData,
+          [event.target.name]: event.target.value,
+        });
+      };
+    
+      const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+          await axios.post("/dj-rest-auth/registration/", signUpData);
+          history.push("/signin");
+        } catch (err) {
+          setErrors(err.response?.data);
+        }
+      };
+
   return (
     <Row className={styles.Row}>
       <Col className="my-auto py-2 p-md-2" md={{ span: 6, offset: 2 }}>
         <Container className="p-4">
           <h1 className={styles.Header}>sign up</h1>
 
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
               <Form.Label className="d-none">username</Form.Label>
               <Form.Control
@@ -22,8 +51,15 @@ const SignUpForm = () => {
                 type="text"
                 placeholder="username"
                 name="username"
+                value={username}
+                onChange={handleChange}
               />
             </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
 
             <Form.Group controlId="password1">
               <Form.Label className="d-none">password</Form.Label>
@@ -32,8 +68,15 @@ const SignUpForm = () => {
                 type="password"
                 placeholder="password"
                 name="password1"
+                value={password1}
+                onChange={handleChange}
               />
             </Form.Group>
+            {errors.password1?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
 
             <Form.Group controlId="password2">
               <Form.Label className="d-none">confirm password</Form.Label>
@@ -42,14 +85,27 @@ const SignUpForm = () => {
                 type="password"
                 placeholder="confirm password"
                 name="password2"
+                value={password2}
+                onChange={handleChange}
               />
             </Form.Group>
+            {errors.password2?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
 
             <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Light}`} type="submit">
               Sign up
             </Button>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
           </Form>
         </Container>
+        
         <Container className="mt-3">
           <Link className={styles.Link} to="/signin">
             Already have an account? <span>Sign in</span>
