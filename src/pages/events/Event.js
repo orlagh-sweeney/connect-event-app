cf253302b6091f4f8f06453ad482882842a409d5
orlagh-sweeney <orlagh16@gmail.com>
2023-09-17 13:40:42 +0000

    Add conditional rendering to Event component

--- src/pages/events/Event.js
+++ src/pages/events/Event.js
@@ -18,7 +18,6 @@ const Event = (props) => {
     comments_count,
     attending_count,
     attending_id,
-    like_id,
     title,
     content,
     image,
@@ -26,7 +25,6 @@ const Event = (props) => {
     time,
     location,
     type,
-    updated_at,
     eventPage,
     setEvents,
   } = props;
@@ -43,7 +41,11 @@ const Event = (props) => {
         ...prevEvents,
         results: prevEvents.results.map((event) => {
           return event.id === id
-            ? { ...event, attending_count: event.attending_count + 1, attending_id: data.id }
+            ? {
+                ...event,
+                attending_count: event.attending_count + 1,
+                attending_id: data.id,
+              }
             : event;
         }),
       }));
@@ -51,7 +53,7 @@ const Event = (props) => {
       console.log(err);
     }
   };
-  
+
   // allows users to unattend an event
   const handleUnattend = async () => {
     try {
@@ -60,7 +62,11 @@ const Event = (props) => {
         ...prevEvents,
         results: prevEvents.results.map((event) => {
           return event.id === id
-            ? { ...event, attending_count: event.attending_count - 1, attending_id: null }
+            ? {
+                ...event,
+                attending_count: event.attending_count - 1,
+                attending_id: null,
+              }
             : event;
         }),
       }));
@@ -71,92 +77,296 @@ const Event = (props) => {
 
   return (
     <Card className={styles.Event}>
-      <Link to={`/events/${id}`}>
-        <Card.Img src={image} alt={title} />
-      </Link>
-      <Card.Body className={styles.Body}>
-        <Media className="align-items-center justify-content-between">
-          <div className="d-flex align-items-center">
-            <span>
-              {date}: {time} CEST
-            </span>
-          </div>
-          <div className="d-flex align-items-center">
-            <span>Attendees: {attending_count}</span>
-          </div>
-        </Media>
-      </Card.Body>
-      <Card.Body className={styles.Body}>
-        <Media className="align-items-center justify-content-between">
-          <div>
-            {title && <Card.Title className="text-left">{title}</Card.Title>}
-            {location && (
-              <Card.Text className="text-left">{location}</Card.Text>
-            )}
-          </div>
-        </Media>
-      </Card.Body>
-      <Card.Body className={styles.Body}>
-        <Media className="align-items-center justify-content-between">
-          <div className="d-flex align-items-center">
-            <Button className={`${btnStyles.Type}`}>
-              {type}
-            </Button>
-          </div>
-          <div>
-            {is_owner ? (
-              <OverlayTrigger
-                placement="top"
-                overlay={
-                  <Tooltip>You cannot de-register your own event.</Tooltip>
-                }
-              >
-                <Button className={`${btnStyles.Button} ${btnStyles.Light}`}>
-                  Attending
-                </Button>
-              </OverlayTrigger>
-            ) : attending_id ? (
-              <OverlayTrigger
-                placement="top"
-                overlay={<Tooltip>Click to unattend</Tooltip>}
-              >
-                <Button
-                  className={`${btnStyles.Button} ${btnStyles.Light}`}
-                  onClick={handleUnattend}
-                >
-                  Attending
-                </Button>
-              </OverlayTrigger>
-            ) : currentUser && !attending_id ? (
-              <OverlayTrigger
-                placeement="top"
-                overlay={<Tooltip>Click to attend</Tooltip>}
-              >
-                <Button
-                  className={`${btnStyles.Button} ${btnStyles.Dark}`}
-                  onClick={handleAttend}
-                >
-                  Not attending
-                </Button>
-              </OverlayTrigger>
-            ) : (
-              <OverlayTrigger
-                placeement="top"
-                overlay={<Tooltip>Log in to attend this evnet</Tooltip>}
-              >
-                <Button
-                  className={`${btnStyles.Button} ${btnStyles.Dark}`}
-                  onClick={() => {}}
-                >
-                  Not attending
-                </Button>
-              </OverlayTrigger>
-            )}
-          </div>
-        </Media>
-      </Card.Body>
+      {eventPage ? (
+        <div className="px-4">
+          <Card.Body className={styles.Body}>
+            <Media className="align-items-center justify-content-between">
+              <div className="d-flex align-items-center">
+                <span>
+                  <h1>{title}</h1>
+                </span>
+              </div>
+              <div className="d-flex align-items-center">
+                <span>delete/edit button</span>
+              </div>
+            </Media>
+          </Card.Body>
+          <Link to={`/events/${id}`}>
+            <Card.Img src={image} alt={title} />
+          </Link>
+          <Card.Body className={styles.Body}>
+            <Media className="align-items-center justify-content-between">
+              <div className="d-flex align-items-center">
+                <span>
+                  {date}: {time}
+                </span>
+              </div>
+              <div className="d-flex align-items-center">
+                <span>
+                  Organiser: <Link to={`/profiles/${profile_id}`}>{owner}</Link>
+                </span>
+              </div>
+            </Media>
+          </Card.Body>
+          <Card.Body className={styles.Body}>
+            <Media className="align-items-center justify-content-between">
+              <div className="d-flex align-items-center">
+                <span>Location: {location}</span>
+              </div>
+              <div className="d-flex align-items-center">
+                <span>Attendees: {attending_count}</span>
+              </div>
+            </Media>
+          </Card.Body>
+          <Card.Body className={styles.Body}>
+            <Media>
+              <div>
+                <span>{content}</span>
+              </div>
+            </Media>
+          </Card.Body>
+          <Card.Body className={styles.Body}>
+            <Media className="align-items-center justify-content-between">
+              <div className="d-flex align-items-center">
+                <Button className={`${btnStyles.Type}`}>{type}</Button>
+              </div>
+              <div>
+                {is_owner ? (
+                  <OverlayTrigger
+                    placement="top"
+                    overlay={
+                      <Tooltip>You cannot de-register your own event.</Tooltip>
+                    }
+                  >
+                    <Button
+                      className={`${btnStyles.Button} ${btnStyles.Light}`}
+                    >
+                      Attending
+                    </Button>
+                  </OverlayTrigger>
+                ) : attending_id ? (
+                  <OverlayTrigger
+                    placement="top"
+                    overlay={<Tooltip>Click to unattend</Tooltip>}
+                  >
+                    <Button
+                      className={`${btnStyles.Button} ${btnStyles.Light}`}
+                      onClick={handleUnattend}
+                    >
+                      Attending
+                    </Button>
+                  </OverlayTrigger>
+                ) : currentUser && !attending_id ? (
+                  <OverlayTrigger
+                    placeement="top"
+                    overlay={<Tooltip>Click to attend</Tooltip>}
+                  >
+                    <Button
+                      className={`${btnStyles.Button} ${btnStyles.Dark}`}
+                      onClick={handleAttend}
+                    >
+                      Not attending
+                    </Button>
+                  </OverlayTrigger>
+                ) : (
+                  <OverlayTrigger
+                    placeement="top"
+                    overlay={<Tooltip>Log in to attend this evnet</Tooltip>}
+                  >
+                    <Button
+                      className={`${btnStyles.Button} ${btnStyles.Dark}`}
+                      onClick={() => {}}
+                    >
+                      Not attending
+                    </Button>
+                  </OverlayTrigger>
+                )}
+              </div>
+            </Media>
+          </Card.Body>
+        </div>
+      ) : (
+        <div>
+          <Link to={`/events/${id}`}>
+            <Card.Img src={image} alt={title} />
+          </Link>
+          <Card.Body className={styles.Body}>
+            <Media className="align-items-center justify-content-between">
+              <div className="d-flex align-items-center">
+                <span>
+                  {date}: {time}
+                </span>
+              </div>
+              <div className="d-flex align-items-center">
+                <span>Attendees: {attending_count}</span>
+              </div>
+            </Media>
+          </Card.Body>
+          <Card.Body className={styles.Body}>
+            <Media className="align-items-center justify-content-between">
+              <div>
+                {title && (
+                  <Card.Title className="text-left">{title}</Card.Title>
+                )}
+                {location && (
+                  <Card.Text className="text-left">{location}</Card.Text>
+                )}
+              </div>
+            </Media>
+          </Card.Body>
+          <Card.Body className={styles.Body}>
+            <Media className="align-items-center justify-content-between">
+              <div className="d-flex align-items-center">
+                <Button className={`${btnStyles.Type}`}>{type}</Button>
+              </div>
+              <div>
+                {is_owner ? (
+                  <OverlayTrigger
+                    placement="top"
+                    overlay={
+                      <Tooltip>You cannot de-register your own event.</Tooltip>
+                    }
+                  >
+                    <Button
+                      className={`${btnStyles.Button} ${btnStyles.Light}`}
+                    >
+                      Attending
+                    </Button>
+                  </OverlayTrigger>
+                ) : attending_id ? (
+                  <OverlayTrigger
+                    placement="top"
+                    overlay={<Tooltip>Click to unattend</Tooltip>}
+                  >
+                    <Button
+                      className={`${btnStyles.Button} ${btnStyles.Light}`}
+                      onClick={handleUnattend}
+                    >
+                      Attending
+                    </Button>
+                  </OverlayTrigger>
+                ) : currentUser && !attending_id ? (
+                  <OverlayTrigger
+                    placeement="top"
+                    overlay={<Tooltip>Click to attend</Tooltip>}
+                  >
+                    <Button
+                      className={`${btnStyles.Button} ${btnStyles.Dark}`}
+                      onClick={handleAttend}
+                    >
+                      Not attending
+                    </Button>
+                  </OverlayTrigger>
+                ) : (
+                  <OverlayTrigger
+                    placeement="top"
+                    overlay={<Tooltip>Log in to attend this evnet</Tooltip>}
+                  >
+                    <Button
+                      className={`${btnStyles.Button} ${btnStyles.Dark}`}
+                      onClick={() => {}}
+                    >
+                      Not attending
+                    </Button>
+                  </OverlayTrigger>
+                )}
+              </div>
+            </Media>
+          </Card.Body>
+        </div>
+      )}
     </Card>
   );
 };
+
+//   return (
+//     <Card className={styles.Event}>
+//       <Link to={`/events/${id}`}>
+//         <Card.Img src={image} alt={title} />
+//       </Link>
+//       <Card.Body className={styles.Body}>
+//         <Media className="align-items-center justify-content-between">
+//           <div className="d-flex align-items-center">
+//             <span>
+//               {date}: {time} CEST
+//             </span>
+//           </div>
+//           <div className="d-flex align-items-center">
+//             <span>Attendees: {attending_count}</span>
+//           </div>
+//         </Media>
+//       </Card.Body>
+//       <Card.Body className={styles.Body}>
+//         <Media className="align-items-center justify-content-between">
+//           <div>
+//             {title && <Card.Title className="text-left">{title}</Card.Title>}
+//             {location && (
+//               <Card.Text className="text-left">{location}</Card.Text>
+//             )}
+//           </div>
+//         </Media>
+//       </Card.Body>
+//       <Card.Body className={styles.Body}>
+//         <Media className="align-items-center justify-content-between">
+//           <div className="d-flex align-items-center">
+//             <Button className={`${btnStyles.Type}`}>
+//               {type}
+//             </Button>
+//           </div>
+//           <div>
+//             {is_owner ? (
+//               <OverlayTrigger
+//                 placement="top"
+//                 overlay={
+//                   <Tooltip>You cannot de-register your own event.</Tooltip>
+//                 }
+//               >
+//                 <Button className={`${btnStyles.Button} ${btnStyles.Light}`}>
+//                   Attending
+//                 </Button>
+//               </OverlayTrigger>
+//             ) : attending_id ? (
+//               <OverlayTrigger
+//                 placement="top"
+//                 overlay={<Tooltip>Click to unattend</Tooltip>}
+//               >
+//                 <Button
+//                   className={`${btnStyles.Button} ${btnStyles.Light}`}
+//                   onClick={handleUnattend}
+//                 >
+//                   Attending
+//                 </Button>
+//               </OverlayTrigger>
+//             ) : currentUser && !attending_id ? (
+//               <OverlayTrigger
+//                 placeement="top"
+//                 overlay={<Tooltip>Click to attend</Tooltip>}
+//               >
+//                 <Button
+//                   className={`${btnStyles.Button} ${btnStyles.Dark}`}
+//                   onClick={handleAttend}
+//                 >
+//                   Not attending
+//                 </Button>
+//               </OverlayTrigger>
+//             ) : (
+//               <OverlayTrigger
+//                 placeement="top"
+//                 overlay={<Tooltip>Log in to attend this evnet</Tooltip>}
+//               >
+//                 <Button
+//                   className={`${btnStyles.Button} ${btnStyles.Dark}`}
+//                   onClick={() => {}}
+//                 >
+//                   Not attending
+//                 </Button>
+//               </OverlayTrigger>
+//             )}
+//           </div>
+//         </Media>
+//       </Card.Body>
+//     </Card>
+//   );
+// };
 
 export default Event;
