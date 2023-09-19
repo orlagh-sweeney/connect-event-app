d48301111be2a21dc0a4deaf59391d564abc1132
orlagh-sweeney <orlagh16@gmail.com>
2023-09-19 10:29:31 +0000

    Update dropdownmenu component for profilepage

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import styles from "../styles/DropdownMenu.module.css";
+import { useHistory } from "react-router";
 
 // dropdown menu with edit and delete buttons
 const ThreeDots = React.forwardRef(({ onClick }, ref) => (
@@ -36,6 +37,37 @@ const ThreeDots = React.forwardRef(({ onClick }, ref) => (
             aria-label="delete"
           >
             <i className="fas fa-trash-alt" />
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    );
+  };
+
+  export function ProfileEditDropdown({ id }) {
+    const history = useHistory();
+    return (
+      <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
+        <Dropdown.Toggle as={ThreeDots} />
+        <Dropdown.Menu>
+          <Dropdown.Item
+            onClick={() => history.push(`/profiles/${id}/edit`)}
+            aria-label="edit-profile"
+          >
+            <i className="fas fa-edit" /> edit profile
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => history.push(`/profiles/${id}/edit/username`)}
+            aria-label="edit-username"
+          >
+            <i className="far fa-id-card" />
+            change username
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => history.push(`/profiles/${id}/edit/password`)}
+            aria-label="edit-password"
+          >
+            <i className="fas fa-key" />
+            change password
           </Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
