7069c464ea4c31c27b29210f48422d6a3838f65a
orlagh-sweeney <orlagh16@gmail.com>
2023-09-27 18:49:56 +0000

    Add missing semicolon to axoisdefaults

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,8 +4,8 @@ import axios from "axios";
 Code from Code Institute Moments walkthrough project
 */
 
-axios.defaults.baseURL = "https://drf-event-api-8dabfe6ebdec.herokuapp.com/"
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.baseURL = "https://drf-event-api-8dabfe6ebdec.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
