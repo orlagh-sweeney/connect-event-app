84a01e94d8271ab96fc8888fa123601f9d8a2a49
orlagh-sweeney <orlagh16@gmail.com>
2023-09-28 13:30:27 +0000

    Adjust styling of about buttons and format code

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,6 @@ import Container from "react-bootstrap/Container";
 
 // style imports
 import styles from "../styles/About.module.css";
-import btnStyles from "../styles/Button.module.css";
 
 function AboutPage() {
   return (
@@ -25,12 +24,15 @@ function AboutPage() {
       <Container>
         <Row>
           <Col className="text-center mt-4">
-            <h2 className={`${styles.Header} text-center mt-4 pb-2`}>Who are we?</h2>
+            <h2 className={`${styles.Header} text-center mt-4 pb-2`}>
+              Who are we?
+            </h2>
             <p>
-              Connect Bcn is Barcelona&apos;s go-to platform to find out what&apos;s going
-              in this bustling city. Whether you want to experience Barcelona&apos;s
-              rich culture or find some fellow cylists to explore this outskirts
-              of the city, Connect Bcn is here to help.
+              Connect Bcn is Barcelona&apos;s go-to platform to find out
+              what&apos;s going in this bustling city. Whether you want to
+              experience Barcelona&apos;s rich culture or find some fellow
+              cylists to explore this outskirts of the city, Connect Bcn is here
+              to help.
             </p>
           </Col>
         </Row>
@@ -38,30 +40,36 @@ function AboutPage() {
 
       <Container>
         <Row>
-        <Col className="text-center mt-4">
-        <h2 className={`${styles.Header} text-center mt-4 pb-2`}>How does it work?</h2>
+          <Col className="text-center mt-4">
+            <h2 className={`${styles.Header} text-center mt-4 pb-2`}>
+              How does it work?
+            </h2>
             <p>
-              We keep things simple. First things first, create an account. Follow the sign up form link below to create a profile.
-              Once logged in you can build your own event or use our search function to find events you would like to attend.<br /> It&apos;s that easy!
+              We keep things simple. First things first, create an account.
+              Follow the sign up form link below to create a profile. Once
+              logged in you can build your own event or use our search function
+              to find events you would like to attend.
+              <br /> It&apos;s that easy!
             </p>
-            </Col>
+          </Col>
         </Row>
       </Container>
 
       <Container>
-        <h2 className={`${styles.Header} text-center mt-4`}>Ready to get started?</h2>
+        <h2 className={`${styles.Header} text-center mt-4`}>
+          Ready to get started?
+        </h2>
         <Row>
           <Col className="text-center mt-4">
-            <Link to={`/signup/`} className={`${btnStyles.Button} ${btnStyles.Light}`}>Sign Up
-              </Link>
-          </Col>
-          <Col className="text-center mt-4">
-            <Link to={`/`} className={`${btnStyles.Button} ${btnStyles.Light}`}>Discover Events
-              </Link>
+            <Link to={`/signup/`} className={`${styles.Button} p-2 mr-4`}>
+              Sign Up
+            </Link>
+            <Link to={`/`} className={`${styles.Button} p-2 ml-4`}>
+              Discover Events
+            </Link>
           </Col>
         </Row>
       </Container>
-
     </div>
   );
 }
