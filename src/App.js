caf58660f6bc1c9c644deeb520fdce6e14fc8fbf
orlagh-sweeney <orlagh16@gmail.com>
2023-09-29 06:11:35 +0000

    Add code commnets to App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,77 @@
-import styles from './App.module.css';
-import NavBar from './components/NavBar';
-import Container from "react-bootstrap/Container";
+// react imports
 import { Route, Switch } from "react-router-dom";
-import "./api/axiosDefaults";
-import SignUpForm from './pages/auth/SignUpForm';
-import SignInForm from './pages/auth/SignInForm';
-import EventCreateForm from './pages/events/EventCreateForm';
-import EventPage from './pages/events/EventPage';
-import EventsPage from './pages/events/EventsPage';
-import EventEditForm from './pages/events/EventEditForm';
-import ProfilePage from './pages/profiles/ProfilePage';
-import ProfileEditForm from './pages/profiles/ProfileEditForm';
-import UserPasswordForm from './pages/profiles/UserPasswordForm';
-import UsernameForm from './pages/profiles/UsernameForm';
-import {NotificationContainer} from 'react-notifications';
-import 'react-notifications/lib/notifications.css';
-import AboutPage from './pages/About';
 
+// api imports
+import "./api/axiosDefaults";
+
+// style imports
+import styles from "./App.module.css";
+
+// bootstrap imports
+import Container from "react-bootstrap/Container";
+
+// component imports
+import NavBar from "./components/NavBar";
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
+import EventCreateForm from "./pages/events/EventCreateForm";
+import EventPage from "./pages/events/EventPage";
+import EventsPage from "./pages/events/EventsPage";
+import EventEditForm from "./pages/events/EventEditForm";
+import ProfilePage from "./pages/profiles/ProfilePage";
+import ProfileEditForm from "./pages/profiles/ProfileEditForm";
+import UserPasswordForm from "./pages/profiles/UserPasswordForm";
+import UsernameForm from "./pages/profiles/UsernameForm";
+import AboutPage from "./pages/About";
+
+// notifications imports
+import { NotificationContainer } from "react-notifications";
+import "react-notifications/lib/notifications.css";
 
 function App() {
-
   return (
     <div className={styles.App}>
       <NavBar />
-      <NotificationContainer/>
+      <NotificationContainer />
       <Container className={styles.Main}>
-      <Switch>
-          <Route exact path="/" render={() => <EventsPage message="No results found. Adjust your search query." />} />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <EventsPage message="No results found. Adjust your search query." />
+            )}
+          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/about" render={() => <AboutPage />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route exact path="/events/create" render={() => <EventCreateForm />} />
+          <Route
+            exact
+            path="/events/create"
+            render={() => <EventCreateForm />}
+          />
           <Route exact path="/events/:id" render={() => <EventPage />} />
-          <Route exact path="/events/:id/edit" render={() => <EventEditForm />} />
+          <Route
+            exact
+            path="/events/:id/edit"
+            render={() => <EventEditForm />}
+          />
           <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
-          <Route exact path="/profiles/:id/edit" render={() => <ProfileEditForm />} />
-          <Route exact path="/profiles/:id/edit/password" render={() => <UserPasswordForm />} />
-          <Route exact path="/profiles/:id/edit/username" render={() => <UsernameForm />} />
+          <Route
+            exact
+            path="/profiles/:id/edit"
+            render={() => <ProfileEditForm />}
+          />
+          <Route
+            exact
+            path="/profiles/:id/edit/password"
+            render={() => <UserPasswordForm />}
+          />
+          <Route
+            exact
+            path="/profiles/:id/edit/username"
+            render={() => <UsernameForm />}
+          />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
